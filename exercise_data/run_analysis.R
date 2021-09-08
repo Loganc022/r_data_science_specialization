8672651865e1706e37fa15233054dc519d6ae3b2
Logan Crain <logan.crain@jci.com>
2021-09-08 14:19:21 -0500

    Fixed assignment data

--- exercise_data/run_analysis.R
+++ exercise_data/run_analysis.R
@@ -9,37 +9,56 @@
 #     data set with the average of each variable for each activity and each subject.
 
 # Step 1: Load data into environment assuming files on working dir
-data_train_x <- read.table("train/X_train.txt")
-data_train_y <- read.table("train/y_train.txt")
-data_test_x <- read.table("test/X_test.txt")
-data_test_y <- read.table("test/y_test.txt")
-feature_labels <- read.table("features.txt")
-activity_labels <- read.table("activity_labels.txt")
+feature_labels <- read.table("features.txt", col.names=c("num","type"))
+data_train_x <- read.table("train/X_train.txt", col.names=feature_labels$type)
+data_train_y <- read.table("train/y_train.txt", col.names="activity_key")
+data_test_x <- read.table("test/X_test.txt", col.names=feature_labels$type)
+data_test_y <- read.table("test/y_test.txt", col.names="activity_key")
+
+activity_labels <- read.table("activity_labels.txt", col.names=c("activity_key", 
+                              "activity"))
+data_test_subject <- read.table("test/subject_test.txt", col.names="subject")
+data_train_subject <- read.table("train/subject_train.txt", col.names="subject")
+
 names(activity_labels) <- c("activity_key","activity")
 
 # Step 2: Merge 4 sets of data into one table
-names(data_train_x) <- feature_labels[,2]
-names(data_test_x) <- feature_labels[,2]
-names(data_train_y) <- "activity_key"
-names(data_test_y) <- "activity_key"
-data_train <- cbind(data_train_x, data_train_y)
-data_test <- cbind(data_test_x, data_test_y)
-data_full <- rbind(data_train,data_test)
-data_mean_std <- data_full %>% select(grep("mean\\(\\)-",names(data_full)),
-                                      grep("std\\(\\)-", names(data_full)),
-                                      grep("activity_key", names(data_full))
+data_x <- rbind(data_train_x, data_test_x)
+data_y <- rbind(data_train_y, data_test_y)
+data_subject <- rbind(data_train_subject, data_test_subject)
+data_full <- cbind(data_subject, data_x, data_y)
+data_mean_std <- data_full %>% select(subject,
+                                      grep("mean",names(data_full)),
+                                      grep("std", names(data_full)),
+                                      activity_key
                                       )
 
 # Step 3: Use descriptive activity names instead of activity_key
 data_mean_std_labeled <- merge(data_mean_std,activity_labels,by="activity_key")
 data_mean_std_labeled <- subset(data_mean_std_labeled, select = -activity_key)
 
-# Step 4: Relabel columns with _ instead of - and remove ()
-colnames(data_mean_std_labeled) <- gsub("\\(\\)","",colnames(data_mean_std_labeled))
-colnames(data_mean_std_labeled) <- gsub("-","_",colnames(data_mean_std_labeled))
+# Step 4: Relabel columns
+names(data_mean_std_labeled)<-gsub("Acc","Accelerometer", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("Gyro", "Gyroscope", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("BodyBody", "Body", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("Mag","Magnitude", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("^t","Time", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("^f","Frequency", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("tBody","TimeBody", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("-mean()","Mean", names(data_mean_std_labeled),
+                                   ignore.case = TRUE)
+names(data_mean_std_labeled)<-gsub("-std()","STD", names(data_mean_std_labeled),
+                                   ignore.case = TRUE)
+names(data_mean_std_labeled)<-gsub("-freq()","Frequency", names(data_mean_std_labeled),
+                                   ignore.case = TRUE)
+names(data_mean_std_labeled)<-gsub("angle","Angle", names(data_mean_std_labeled))
+names(data_mean_std_labeled)<-gsub("grav","Gravity", names(data_mean_std_labeled))
+
 
 # Step 5: Create new data set with average of each feature based on activity:
 average_set <- data_mean_std_labeled %>% 
-                group_by(activity) %>% 
-                summarize(across(tBodyAcc_mean_X:fBodyGyro_std_Z,mean))
+                group_by(subject, activity) %>% 
+                summarize_all(mean)
+write.table(average_set, "step_5_average_set.txt", row.name=FALSE)
+
 
